bf076808bf5d239127977d359afae64b0a7f5325
Jacinto Mba Cantero <jacmba@gmail.com>
2024-04-19 15:26:12 +0100

    Unified configuration and app class

--- src/main/java/net/jazbelt/springboot3training/game/GameApp.java
+++ src/main/java/net/jazbelt/springboot3training/game/GameApp.java
@@ -1,10 +1,45 @@
 package net.jazbelt.springboot3training.game;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
+@Configuration
 public class GameApp {
+    @Bean
+    public IGame marioGame() {
+        return new MarioGame();
+    }
+
+    @Bean
+    public IGame sfGame() {
+        return new SFGame();
+    }
+
+    @Bean
+    public IGame tetrisGame() {
+        return new TetrisGame();
+    }
+
+    @Bean
+    @Primary
+    public GameRunner marioRunner(IGame marioGame) {
+        return new GameRunner(marioGame);
+    }
+
+    @Bean
+    public GameRunner sfRunner(IGame sfGame) {
+        return new GameRunner(sfGame);
+    }
+
+    @Bean
+    public GameRunner tetrisRunner(IGame tetrisGame) {
+        return new GameRunner(tetrisGame);
+    }
+
     public static void main(String[] args) {
-        try(var ctx = new AnnotationConfigApplicationContext(GameConfiguration.class)) {
+        try(var ctx = new AnnotationConfigApplicationContext(GameApp.class)) {
             var marioRunner = (GameRunner) ctx.getBean("marioRunner");
             var sfRunner = (GameRunner) ctx.getBean( "sfRunner");
             var tetrisRunner = (GameRunner) ctx.getBean("tetrisRunner");
