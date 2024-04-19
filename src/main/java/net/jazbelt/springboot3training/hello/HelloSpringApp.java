fb5c5a4e8c31b0c013154e925d20cdab0b3be0d9
Jacinto Mba Cantero <jacmba@gmail.com>
2024-04-19 12:42:13 +0100

    Solving multiple matching beans

--- src/main/java/net/jazbelt/springboot3training/hello/HelloSpringApp.java
+++ src/main/java/net/jazbelt/springboot3training/hello/HelloSpringApp.java
@@ -2,6 +2,8 @@ package net.jazbelt.springboot3training.hello;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import java.util.Arrays;
+
 public class HelloSpringApp {
     public static void main(String[] args) {
         AnnotationConfigApplicationContext ctx =
@@ -13,6 +15,12 @@ public class HelloSpringApp {
         System.out.println(ctx.getBean("person"));
         System.out.println(ctx.getBean("person2MethodCall"));
         System.out.println(ctx.getBean("person3Parameters"));
+        System.out.println(ctx.getBean(Person.class));
+        System.out.println(ctx.getBean("person5Qualifier"));
         System.out.println(ctx.getBean("address1"));
+        System.out.println(ctx.getBean(Address.class));
+
+        Arrays.stream(ctx.getBeanDefinitionNames())
+                .forEach(System.out::println);
     }
 }
--- src/main/java/net/jazbelt/springboot3training/hello/HelloSpringConfig.java
+++ src/main/java/net/jazbelt/springboot3training/hello/HelloSpringConfig.java
@@ -1,7 +1,9 @@
 package net.jazbelt.springboot3training.hello;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 record Person (String name, int age, Address address) {};
 record Address (String firstLine, String city) {};
@@ -34,12 +36,25 @@ public class HelloSpringConfig {
         return new Person(name, age, address3);
     }
 
+    @Bean
+    @Primary
+    public Person person4(String name, int age, Address address) {
+        return new Person(name, age, address);
+    }
+
+    @Bean
+    public Person person5Qualifier(String name, int age, @Qualifier("address3qualifier")Address address) {
+        return new Person(name, age, address);
+    }
+
     @Bean(name = "address1")
+    @Primary
     public Address address() {
         return new Address("Baker Street", "London");
     }
 
     @Bean
+    @Qualifier("address3qualifier")
     public Address address3() {
         return new Address("Sunset Blvd", "LA");
     }
