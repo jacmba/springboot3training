74f2b39106a041cecbd86784fc3553f9cb0e4e75
Jacinto Mba Cantero <jacmba@gmail.com>
2024-04-19 09:05:11 +0100

    Initial commit

---
+++ src/main/java/net/jazbelt/springboot3training/Springboot3trainingApplication.java
@@ -0,0 +1,13 @@
+package net.jazbelt.springboot3training;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot3trainingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot3trainingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/jazbelt/springboot3training/Springboot3trainingApplicationTests.java
@@ -0,0 +1,13 @@
+package net.jazbelt.springboot3training;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot3trainingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
